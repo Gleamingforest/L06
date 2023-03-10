01aaa92749e8d900ff0d67b5d6f9d2ce42821e46
Gleamingforest <108047329+Gleamingforest@users.noreply.github.com>
2023-03-11 05:40:06 +0800

    Update compiletop.sh

--- dynamic/compiletop.sh
+++ dynamic/compiletop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PATH=/Users/arielmotsenyat/Desktop/School  /home/guo99/L06/dynamic
+PATH=/home/guo99/L06/dynamic
 if [[ $1 = '$PATH' ]] ; then
     gcc -L. -Wl,-rpath='$PATH' -Wall -o test top.c  -lLab6Part5
+    exist 1
 elif [[ $1 -eq 0 ]] ; then
     echo "Please provide the path to the shared library file in order to compile the code"
 fi
